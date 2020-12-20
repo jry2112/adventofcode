75b01417f44c6bd7fb087a9c0a36222f1663f7a3
Jada Young <71796163+younjada@users.noreply.github.com>
2020-12-20 11:10:45 -0500

    day 10, day 11 started

--- day11.py
+++ day11.py
@@ -1,21 +1,94 @@
+import copy
+
 def parse(file):
     with open(file) as f:
         input_list = [line.rstrip('\n') for line in f]
-    return input_list
+    clean_list = []
+    for row in range(0, len(input_list)):
+        row_of_seats = []
+        for column in range(0, len(input_list[row])):
+            char = input_list[row][column]
+            row_of_seats.append(char)
+            if len(row_of_seats) == len(input_list[column]):
+                clean_list.append(row_of_seats)
+    return clean_list
+
 
 
 class SeatingSystem:
 
     def __init__(self, input):
-        self.occupied_seat = "L"
+        self.empty_seat = "L"
+        self.occupied_seat = "#"
         self.floor = "."
         self.original_seat_list = parse(input)
+        self.seat_list = copy.deepcopy(self.original_seat_list)
+        self.row = 0
+        self.column = 0
+
+
 
     def display_original_seat_layout(self):
         for row in range(0, len(self.original_seat_list)):
             print(self.original_seat_list[row])
 
-    def update_seats(self):
+    def display_clean_seat_layout(self):
+        for row in range(0, len(self.seat_list)):
+            print(self.seat_list[row])
 
-test_original = SeatingSystem('day11test')
-test_original.display_original_seat_layout()
+    def get_current_seat_layout(self):
+        return self.seat_list
+
+    def update_seat(self, row, column):
+        seat = self.seat_list[row][column]
+        if seat == self.floor:
+            seat = seat
+        elif seat == self.occupied_seat:
+            #check adjacent
+            neighboring_seats = self.find_adjacent_seats(row, column)
+            seat = self.occupied_seat_update(seat, neighboring_seats)
+
+        elif seat == self.empty_seat:
+            neighboring_seats = self.find_adjacent_seats(row, column)
+            seat = self.empty_seat_update(seat, neighboring_seats)
+        return seat
+
+    def find_adjacent_seats(self, row, column):
+        adjacent_list = []
+        for row_index in range(row-1, row + 2):
+            for column_index in range(column-1, column+2):
+                if row_index >= 0 and row_index < len(self.original_seat_list):
+                    if column_index >= 0 and column_index < len(self.original_seat_list[row_index]):
+                        adjacent_list.append(self.seat_list[row_index][column_index])
+        return adjacent_list
+
+    def empty_seat_update(self, seat, adjacency_list):
+        if seat == self.empty_seat:
+            if self.occupied_seat not in adjacency_list:
+                seat == self.occupied_seat
+        return seat
+
+    def occupied_seat_update(self, seat, adjacency_list):
+        if seat == self.occupied_seat:
+            count = 0
+            for spot in adjacency_list:
+                if spot == self.occupied_seat:
+                    count += 1
+            if count >= 4:
+                seat == self.empty_seat
+        return seat
+
+test = SeatingSystem('day11test')
+original_list = test.original_seat_list
+test.display_original_seat_layout()
+print()
+test.display_clean_seat_layout()
+
+new_layout = test.seat_list
+for row in range(0, len(original_list)):
+    for column in range(0, len(original_list[row])):
+        new_layout[row][column] = test.update_seat(row, column)
+test.seat_list = new_layout
+
+print()
+test.display_clean_seat_layout()
